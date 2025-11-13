9943d145001b6a049ad82a249900f892cedd56b3
otaaaa <tomoaki.ota14@gmail.com>
2025-11-13 15:12:07 +0900

    Add descriptions to variables and outputs for Terraform module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "github_oidc_sa_email" {
-  value = google_service_account.github_oidc.email
+  description = "The email address of the service account used by GitHub Actions via Workload Identity Federation."
+  value       = google_service_account.github_oidc.email
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,29 @@
 variable "project_id" {
-  type = string
+  description = "The ID of the Google Cloud project where the Workload Identity Pool and provider will be created."
+  type        = string
 }
 
 variable "pool_id" {
-  type = string
+  description = "The ID of the Workload Identity Pool used to authenticate GitHub Actions via OpenID Connect."
+  type        = string
 }
 
 variable "provider_id" {
-  type = string
+  description = "The ID of the Workload Identity Provider within the pool that trusts GitHub's OIDC tokens."
+  type        = string
 }
 
 variable "roles" {
-  type = list(string)
+  description = "A list of IAM roles to grant to the GitHub service account that will be used by GitHub Actions workflows."
+  type        = list(string)
 }
 
 variable "github_user" {
-  type = string
+  description = "The GitHub organization or user name that owns the repositories authorized to use the OIDC provider."
+  type        = string
 }
 
 variable "github_repositories" {
-  type = list(string)
+  description = "A list of GitHub repository names under the specified user/organization that are allowed to authenticate using this Workload Identity Provider."
+  type        = list(string)
 }
